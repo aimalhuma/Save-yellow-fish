4cb43e1c2acb33d8996e88229120e1ff83f99973
aimalhuma61 <66478525+aimalhuma61@users.noreply.github.com>
2020-07-18 17:57:52 +0500

    add some animations to fishes

--- src/App.js
+++ src/App.js
@@ -7,46 +7,153 @@ import useWebAnimations from "@wellyshen/use-web-animations";
 
 
 function App() {
-  const { ref, playState } = useWebAnimations
-   ({
-    keyframes: [          
-      { transform: 'translateX(100%)' },
-      { transform: 'translateX(-100%)' },
-    //   { transform: 'translate(0,50px)' },
-    // { transform: 'translate(550px,80px)' },
-    { transform: 'translate(100VW,20px)' },
-    // { transform: 'translate(550px,80px)' },
-    // { transform: 'translate(0,20px)' },
+  const { ref, playState ,getAnimation} = useWebAnimations
+  ({
+    keyframes: [       
+      
+      {transform: 'translate(0px)'},
+      {transform: 'translate(-2400px)'},
+   
     ],
     timing: {
-      delay: 500, // Start with a 500ms delay
-      duration: 2000, // Run for 1000ms
-      iterations: 4, // Repeat once
-      direction: "reverse", // Run the animation forwards and then backwards
-      easing: "ease-in-out", // Use a fancy timing function
+      delay: 2, // Start with a 500ms delay
+      duration: 2500, // Run for 1000ms
+      iterations: 1, // Repeat once
+      // direction: "reverse", // Run the animation forwards and then backwards
+      easing: "linear", // Use a fancy timing function
       
     },
-  //   onReady: ({ playState, animate, animation }) => {
-  //     // Triggered when the animation is ready to play (Google Chrome: available in v84+)
-  //   },
-  //   onUpdate: ({ playState, animate, animation }) => {
-  //     // Triggered when the animation enters the running state or changes state
-  //   },
-  //   onFinish: ({ playState, animate, animation }) => {
-  //     // Triggered when the animation enters the finished state (Google Chrome: available in v84+)
-  
-  //   // More useful options...
-  // });
+}, [])
+  const fish1= useWebAnimations
+  ({
+    keyframes: [       
+      
+      {transform: 'translate(0px)'},
+      {transform: 'translate(-2400px)'},
+   
+    ],
+    timing: {
+      delay: 2, // Start with a 500ms delay
+      duration: 2500, // Run for 1000ms
+      iterations: 1, // Repeat once
+      // direction: "reverse", // Run the animation forwards and then backwards
+      easing: "linear", // Use a fancy timing function
+      
+    },
+}, [])
+const frightfish = useWebAnimations
+   ({
+    keyframes: [       
+      
+      {transform: 'rotate(360deg)'},
+      {transform: 'rotate(0deg)'},
+      {transform: 'translate(0px,-400px)'},
+      {transform: 'translate(600px,-200px)'},
+      {transform: 'translate(1000px,200px)'},
+      {transform: 'translate(1200px,400px)'},
+      {transform: 'translate(100px,300px)'},
+     
+     
+    ],
+    timing: {
+      delay: 2, // Start with a 500ms delay
+      duration: 2500, // Run for 1000ms
+      iterations: 1, // Repeat once
+      // direction: "reverse", // Run the animation forwards and then backwards
+      easing: "linear", // Use a fancy timing function
+      
+    },
+}, [])
+const groupfish = useWebAnimations
+   ({
+    keyframes: [       
+      
+      {transform: 'rotate(360deg)'},
+      {transform: 'rotate(0deg)'},
+      {transform: 'translate(0px,-400px)'},
+      {transform: 'translate(600px,-200px)'},
+      {transform: 'translate(1000px,200px)'},
+      {transform: 'translate(1200px,400px)'},
+      {transform: 'translate(100px,300px)'},
+     
+     
+    ],
+    timing: {
+      delay: 2, // Start with a 500ms delay
+      duration: 2500, // Run for 1000ms
+      iterations: 1, // Repeat once
+      // direction: "reverse", // Run the animation forwards and then backwards
+      easing: "linear", // Use a fancy timing function
+      
+    },
+}, [])
+const yellowgrp = useWebAnimations
+   ({
+    keyframes: [       
+      
+      {transform: 'rotate(360deg)'},
+      {transform: 'rotate(0deg)'},
+      {transform: 'translate(0px,-400px)'},
+      {transform: 'translate(600px,-200px)'},
+      {transform: 'translate(1000px,200px)'},
+      {transform: 'translate(1200px,400px)'},
+      {transform: 'translate(100px,300px)'},
+     
+     
+    ],
+    timing: {
+      delay: 2, // Start with a 500ms delay
+      duration: 2500, // Run for 1000ms
+      iterations: 1, // Repeat once
+      // direction: "reverse", // Run the animation forwards and then backwards
+      easing: "linear", // Use a fancy timing function
+      
+    },
+}, [])
+const shark1 = useWebAnimations
+   ({
+    keyframes: [       
+      
+      {transform: 'translate(0px)'},
+      {transform: 'translate(-2400px)'},
+   
+    ],
+    timing: {
+      delay: 2, // Start with a 500ms delay
+      duration: 3500, // Run for 1000ms
+      iterations: 1, // Repeat once
+      // direction: "reverse", // Run the animation forwards and then backwards
+      easing: "linear", // Use a fancy timing function
+      
+    },
 }, [])
   return (
     <div className="container"  style={{ backgroundImage: "url('./images/bakgrndimg.gif')" } } >
-      <div >
+      <div className="fish1">
         {/* <img src="./bakgrndimg.gif" height="100%" width="100%" /> */}
-     
-        
-       
-        
-       <img src="./images/fish1.gif" className="target" ref={ref}/> 
+       <img src="./images/fish1.gif" className="target" ref={fish1.ref}/> 
+       </div >
+       <div className="frightfish">
+         <img src="./images/frightingfish.gif" className="target" ref={frightfish.ref}/>
+         </div>
+       <div className="shark">
+         <img src="./images/shark6.gif" height="60%" className="target" ref={shark1.ref}/>
+         </div>
+         <div className="reef">
+         <img src="./images/reef3.png" height="80%" width="150%"/>
+         </div>
+         <div className="reef2" >
+         <img src="./images/reef4.png" height="80%" width="150%"/>
+         </div>
+         <div className="reef2" >
+         <img src="./images/groupp.gif" height="80%" width="150%" ref={groupfish.ref}/>
+         </div>
+         <div className="reef2" >
+         <img src="./images/fish7.gif" height="10%" width="10%" ref={fish1.ref}/>
+         </div>
+         <div className="reef2" >
+         <img src="./images/fish2.gif" height="10%" width="10%" ref={yellowgrp.ref}/>
+         </div>
         {/* <img src="./images/groupp.gif" />
         <img src="./images/fish7.gif" />
         <img src="./images/fish2.gif" />
@@ -61,7 +168,7 @@ function App() {
        
         
       
-      </div>
+    
 
 
     </div>
